0a3db56c0b031fef0a76c7dcfbe797784000918d
HyeonminLEE <pfr0213@gmail.com>
2026-06-28 08:24:39 +0000

    fix: 세그멘테이션 오류 수정 - 이모지가 네이티브 메모리를 손상시키는 문제가 있다고 함 test: 세척수 지그 위치 안착시 x방향 힘 수정

--- code/robot_ws/src/auto_dump_robot_pkg/auto_dump_robot_pkg/motion_controller.py
+++ code/robot_ws/src/auto_dump_robot_pkg/auto_dump_robot_pkg/motion_controller.py
@@ -85,7 +85,7 @@ ROBOT_CONTROLLER_SAFETY_STATES = {3, 5, 6, 8, 9, 10, 15}
 
 # 외력제어 파라미터
 FORCE_TH = 20.0  # place시 외력감지 Threshold
-DESIRED_FORCE_X = 0.0     # 세척 위치 안착 방향 힘[N] - 베이스 좌표계 +X 방향
+DESIRED_FORCE_X = 30.0     # 세척 위치 안착 방향 힘[N] - 베이스 좌표계 +X 방향
 DESIRED_FORCE_Z = 10.0     # 세척 위치 Z방향 힘[N] - 실기 테스트 후 조정
 COMPLIANCE_X = 300         # X 순응 강성 - 낮을수록 +X 방향 접촉면을 부드럽게 따라감
 COMPLIANCE_Y = 3000        # Y 순응 강성 - Y방향의 불필요한 움직임을 억제
